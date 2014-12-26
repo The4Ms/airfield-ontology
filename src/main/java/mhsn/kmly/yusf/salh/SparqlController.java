569773e03f105d2ec7444c94928fee6bcca45366
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-12-26 16:35:06 +0200

    Query Controller: returning the result of the select query in json format in the http response

--- src/main/java/mhsn/kmly/yusf/salh/SparqlController.java
+++ src/main/java/mhsn/kmly/yusf/salh/SparqlController.java
@@ -1,6 +1,7 @@
 package mhsn.kmly.yusf.salh;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,20 +9,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.sparql.resultset.JSONOutput;
+
 @WebServlet(displayName = "SparqlController", urlPatterns = { "/sparql" })
 public class SparqlController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		
+
 		String query = request.getParameter("query");
 
-		//System.out.println(query);
+		ResultSet res = (ResultSet)SparqlHandlerProvider.getSparqlHandler()
+				.excuteQuery(query);
+
+		JSONOutput s = new JSONOutput();
 		
-		Object res = SparqlHandlerProvider.getSparqlHandler().excuteQuery(query);
+		String jsonRes = s.asString(res);
 		
-		System.out.println(res.toString());
+		PrintWriter writer = response.getWriter();
+		writer.print(jsonRes.length() == 0 ? "No data found" : jsonRes); 
+		writer.close();
 		
 	}
 }
