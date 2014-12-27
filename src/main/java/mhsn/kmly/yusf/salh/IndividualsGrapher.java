80b82f8c40082a209d2b2991d5d51492b6ebedf6
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-12-27 13:43:52 +0200

    added a class to get Graph that represents the individuals

---
+++ src/main/java/mhsn/kmly/yusf/salh/IndividualsGrapher.java
@@ -0,0 +1,56 @@
+package mhsn.kmly.yusf.salh;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+
+public class IndividualsGrapher {
+	
+	public static Graph buildIndividualsGraph(OntModel ontModel){
+		if(ontModel == null)
+			throw new NullPointerException(
+					"the ontology model has not been initialized");
+
+		Graph g = new Graph();
+		
+		ExtendedIterator<Individual> individuals = ontModel.listIndividuals();
+		while(individuals.hasNext()){
+			Individual individual = individuals.next();
+			StmtIterator listProperties = individual.listProperties();
+			while(listProperties.hasNext()){
+				Statement statement = listProperties.next();
+				String subject = statement.getSubject().asResource().getLocalName();
+				String predicate = statement.getPredicate().asResource().getLocalName();
+				String object = "";
+				
+				if(statement.getObject().isLiteral()){
+					object = statement.getObject().asLiteral().getValue().toString();
+				}
+				else{
+					object = statement.getObject().asResource().getLocalName();
+				}
+				
+				if(predicate.equals("type"))
+					continue;
+
+				addUniqueNode(g, subject);
+				addUniqueNode(g, object);
+					
+				g.addEdge(subject, object, new GraphEdge(predicate));
+			}
+		}
+		
+		return g;
+	}
+	
+	static void addUniqueNode(Graph g, String nodeName){
+		try{
+			g.getNode(nodeName);
+		}
+		catch(NullPointerException ex){
+			g.addNode(new GraphNode(nodeName, ""));
+		}
+	}
+}
